3e8e34353095cff333625305d41cdf237bccf09f
gabrielvvs1 <gabrielvvs1@gmail.com>
2025-06-23 12:50:45 -0300

    Implementação da funcionalidade Deletar tarefas, Editar tarefas, Atualizar tarefas

--- todoApp/todo_list/urls.py
+++ todoApp/todo_list/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('delete/<int:todo_id>/', views.delete_todo, name='delete_todo'),
+    path('edit/<int:todo_id>/', views.edit_todo, name='edit_todo'),
 ]
--- todoApp/todo_list/views.py
+++ todoApp/todo_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import todo
 from .forms import TodoForm
 
@@ -8,13 +8,29 @@ def home(request):
         form = TodoForm(request.POST or None)
         if form.is_valid():
             form.save()
-            todos = todo.objects.all()
-            return render(request, 'home.html', {'todos': todos})
-    else:
-        todos = todo.objects.all()
-        return render(request, 'home.html', {'todos': todos})
+            return redirect('home')  # Redireciona após adicionar para evitar duplicação
+    todos = todo.objects.all()
+    form = TodoForm()
+    return render(request, 'home.html', {'todos': todos, 'form': form})
 
 def about(request):
     context = {'name': 'Gabriel Victor', 'age': 24}
     return render(request, 'about.html', context)
+
+def delete_todo(request, todo_id):
+    todo_item = get_object_or_404(todo, id=todo_id)
+    todo_item.delete()
+    return redirect('home')
+
+
+def edit_todo(request, todo_id):
+    todo_item = get_object_or_404(todo, id=todo_id)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo_item)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TodoForm(instance=todo_item)
+    return render(request, 'edit_todo.html', {'form': form, 'todo_id': todo_id})
 
